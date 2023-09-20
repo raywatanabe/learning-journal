4f122c0f0867f3db766eb7a2cc4ca035b2baa0c3
raywatanabe <raywatanabe23@gmail.com>
2023-09-19 21:29:33 -0600

    create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+const blogs = ["one", "two", "three", "four", "five", "six"]
+const recentBlogPosts = document.getElementById("recent-blog-posts")
+const viewMore = document.getElementById("view-more")
+
+for (let i=0; i < blogs.length; i++) {
+    recentBlogPosts.innerHTML += 
+    `<div class="blog-post" id="blog-${blogs[i]}">
+        <img src="https://source.unsplash.com/random/720x480/?coding&${i}">
+        <p class="date">September 19, 2023</p>
+        <h3>Blog ${blogs[i]}</h3>
+        <p>I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.</p>
+    </div>`
+}
+
+viewMore.addEventListener("click", function() {
+    document.getElementById("blog-four").style.display = "block"
+    document.getElementById("blog-five").style.display = "block"
+    document.getElementById("blog-six").style.display = "block"
+    viewMore.style.display = "none"
+})
+
+
+
+document.getElementById("copyright").innerHTML = `&copy ${new Date().getFullYear()}`
